33cee43244252dfc02881736c01da7eefe6f431e
lindtsey <giousalvador@gmail.com>
2026-02-03 23:52:15 +0800

    feat: added patient model

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/model/Patient.java
@@ -0,0 +1,69 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "patient_id")
+    private UUID patientId;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "middle_name", nullable = false, length = 50)
+    private String middleName;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Size(max = 10)
+    @NotNull
+    @Column(name = "gender", nullable = false, length = 10)
+    private String gender;
+
+    @Size(max = 15)
+    @NotNull
+    @Column(name = "contact_number", nullable = false, length = 15)
+    private String contactNumber;
+
+    @Email
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "email", nullable = false, length = 100, unique = true)
+    private String email;
+
+    @NotNull
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "is_archived", nullable = false)
+    private Boolean isArchived = false;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
