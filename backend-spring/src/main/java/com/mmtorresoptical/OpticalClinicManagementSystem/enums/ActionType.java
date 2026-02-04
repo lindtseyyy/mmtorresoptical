9d7c68c0e5eb1d326a32f2a3448247c81e256155
lindtsey <giousalvador@gmail.com>
2026-02-05 01:53:55 +0800

    feat: added ActionType enum

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/enums/ActionType.java
@@ -0,0 +1,11 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.enums;
+
+public enum ActionType {
+    CREATE,
+    UPDATE,
+    DELETE,
+    LOGIN,
+    LOGOUT,
+    ARCHIVE,
+    RESTORE
+}
