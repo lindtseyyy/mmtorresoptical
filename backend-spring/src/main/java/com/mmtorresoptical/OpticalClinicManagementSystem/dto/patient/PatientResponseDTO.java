d34a04c7677aeedc436f5c3006287e644e4bc7e7
lindtsey <giousalvador@gmail.com>
2026-02-08 00:11:55 +0800

    feat: added PatientResponseDTO model

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/patient/PatientResponseDTO.java
@@ -0,0 +1,23 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient;
+
+import lombok.Data;
+import org.springframework.boot.convert.DataSizeUnit;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class PatientResponseDTO {
+    private UUID patientId;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String gender;
+    private String contactNumber;
+    private String email;
+    private LocalDate birthDate;
+    private String address;
+    private Boolean isArchived;
+    private LocalDateTime createdAt;
+}
