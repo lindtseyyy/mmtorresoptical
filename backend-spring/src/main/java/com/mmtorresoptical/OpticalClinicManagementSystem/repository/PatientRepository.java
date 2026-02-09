e67c4412243e87f85a3b7949df3afffe5f1d2f20
lindtsey <giousalvador@gmail.com>
2026-02-09 22:14:37 +0800

    feat: added a derived query for checking if a patient exists by their full name

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/PatientRepository.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/PatientRepository.java
@@ -2,6 +2,8 @@ package com.mmtorresoptical.OpticalClinicManagementSystem.repository;
 
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.Patient;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.User;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface PatientRepository extends JpaRepository<Patient, UUID> {
     // For finding patients
     List<Patient> findAllByIsArchivedFalse();
 
+    Optional<Patient> findPatientByFirstNameAndMiddleNameAndLastName(String firstName, String middleName,String lastName);
+
+    Boolean existsByFirstNameHashAndMiddleNameHashAndLastNameHash(String firstNameHash, String middleNameHash,String lastNameHash);
 }
