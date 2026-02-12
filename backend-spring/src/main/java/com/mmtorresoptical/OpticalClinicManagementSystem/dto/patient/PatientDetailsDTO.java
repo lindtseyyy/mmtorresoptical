cfdc82e4e7b0e4c91f90dd193bcb9ad34237b153
lindtsey <giousalvador@gmail.com>
2026-02-12 20:40:24 +0800

    feat: added the remaining details | added Data annotation | added the child data

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/patient/PatientDetailsDTO.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/patient/PatientDetailsDTO.java
@@ -1,15 +1,32 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient;
 
+import com.mmtorresoptical.OpticalClinicManagementSystem.model.HealthHistory;
+import com.mmtorresoptical.OpticalClinicManagementSystem.model.Prescription;
+import com.mmtorresoptical.OpticalClinicManagementSystem.model.Transaction;
+import lombok.Data;
+
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
+@Data
 public class PatientDetailsDTO {
     private UUID patientId;
     private String firstName;
+    private String middleName;
     private String lastName;
     private String gender;
     private String contactNumber;
     private String email;
     private LocalDate birthDate;
     private String address;
+    private Boolean isArchived;
+    private LocalDateTime createdAt;
+
+    // Foreign keys / Child data
+    private Set<Prescription> prescriptions;
+    private Set<HealthHistory> healthHistory;
+    private List<Transaction> transactions;
 }
