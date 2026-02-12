4b94e683ec960f5c149dddd7ee71e500ba3fa594
lindtsey <giousalvador@gmail.com>
2026-02-12 23:12:55 +0800

    feat: added constructor injection

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/controller/HealthHistoryController.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/controller/HealthHistoryController.java
@@ -1,9 +1,22 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.controller;
 
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.healthhistory.HealthRequestDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.repository.HealthHistoryRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/healthhistory")
 public class HealthHistoryController {
+
+    private final HealthHistoryRepository healthHistoryRepository;
+
+    HealthHistoryController(HealthHistoryRepository healthHistoryRepository) {
+        this.healthHistoryRepository = healthHistoryRepository;
+    }
+
 }
