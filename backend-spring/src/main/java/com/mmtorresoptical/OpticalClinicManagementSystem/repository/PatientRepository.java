e4abcefd3e8b2254b3ace04ac3f4d53c1dbd241b
lindtsey <giousalvador@gmail.com>
2026-02-12 20:39:16 +0800

    fix: changed the return to Page fo pagination

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/PatientRepository.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/PatientRepository.java
@@ -1,15 +1,16 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.repository;
 
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PatientRepository extends JpaRepository<Patient, UUID> {
     // For finding patients
-    List<Patient> findAllByIsArchivedFalse();
+    Page<Patient> findAllByIsArchivedFalse(Pageable pageable);
 
     Optional<Patient> findPatientByFirstNameAndMiddleNameAndLastName(String firstName, String middleName,String lastName);
     Optional<Patient> findPatientByEmail(String email);
