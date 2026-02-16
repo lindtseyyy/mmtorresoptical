ca93060cbcb3efbc7cfee20aa1c1019a7ff4e6b5
lindtseyxvii <giousalvador@gmail.com>
2026-02-16 21:16:02 +0800

    feat: integrate HealthHistoryMapper for improved HealthHistory to DTO conversion and add DTO-to-entity update function for Patient

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/PatientMapper.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/PatientMapper.java
@@ -4,23 +4,29 @@ import com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient.PatientDeta
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient.PatientRequestDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient.PatientResponseDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.Patient;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        uses = HealthHistoryMapper.class
+)
 public interface PatientMapper {
 
     @Mapping(
             target = "gender",
             expression = "java(patient.getGender().name())"
     )
-    PatientResponseDTO toResponse(Patient patient);
+    PatientResponseDTO entityToResponse(Patient patient);
 
     @Mapping(
             target = "gender",
             expression = "java(patient.getGender().name())"
     )
-    PatientDetailsDTO toDetails(Patient patient);
+    PatientDetailsDTO entityToDetailedResponse(Patient patient);
 
     Patient dtoToPatient(PatientRequestDTO patientRequest);
+
+    @BeanMapping(nullValuePropertyMappingStrategy =
+            NullValuePropertyMappingStrategy.IGNORE)
+    void updatePatientFromDto(PatientRequestDTO patientRequest, @MappingTarget Patient patient);
 }
