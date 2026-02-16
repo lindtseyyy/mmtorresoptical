a5e62ffb6206579c4e54212144a6eb6cf1a9e93d
lindtseyxvii <giousalvador@gmail.com>
2026-02-17 01:54:29 +0800

    feat: add updateHistoryFromDto mapper function

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/HealthHistoryMapper.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/HealthHistoryMapper.java
@@ -2,9 +2,11 @@ package com.mmtorresoptical.OpticalClinicManagementSystem.mapper;
 
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.healthhistory.HealthHistoryDetailsDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.healthhistory.HealthHistoryResponseDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.healthhistory.UpdateHealthHistoryRequestDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.HealthHistory;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(
         componentModel = "spring",
@@ -31,5 +33,7 @@ public interface HealthHistoryMapper {
             source = "createdAt"
     )
     HealthHistoryDetailsDTO historyToDetailsDTO(HealthHistory healthHistory);
+
+    void updateHistoryFromDTO(UpdateHealthHistoryRequestDTO updateHealthHistoryRequestDTO, @MappingTarget HealthHistory healthHistory);
 
 }
