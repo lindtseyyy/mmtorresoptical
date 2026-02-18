9b1a23d7d44045cf553f8d5f7a6edbc087093243
lindtseyxvii <giousalvador@gmail.com>
2026-02-18 21:41:30 +0800

    refactor: rename to CreatePrescriptionRequestDTO

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/prescription/CreatePrescriptionRequestDTO.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/prescription/CreatePrescriptionRequestDTO.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Data
-public class PrescriptionRequestDTO {
+public class CreatePrescriptionRequestDTO {
 
     @NotNull(message = "Exam date is required")
     private LocalDate examDate;
