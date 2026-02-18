cc5c0a973ab9fde49764a9872477e12607ce4530
lindtseyxvii <giousalvador@gmail.com>
2026-02-18 19:49:33 +0800

    feat: create AuthenticatedUserService feat: add function to retrieve the current authenticated user feat: add function to retrieve the current authenticated user id

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/service/CustomUserDetailsService/AuthenticatedUserService.java
@@ -0,0 +1,47 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.service.CustomUserDetailsService;
+
+import com.mmtorresoptical.OpticalClinicManagementSystem.exception.ResourceNotFoundException;
+import com.mmtorresoptical.OpticalClinicManagementSystem.model.User;
+import com.mmtorresoptical.OpticalClinicManagementSystem.repository.UserRepository;
+import com.mmtorresoptical.OpticalClinicManagementSystem.security.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class AuthenticatedUserService {
+    private final UserRepository userRepository;
+
+    public AuthenticatedUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getCurrentUser() {
+
+        Authentication auth =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        CustomUserDetails principal =
+                (CustomUserDetails) auth.getPrincipal();
+
+        UUID userId = principal.getUserId();
+
+        return userRepository.findById(userId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User not found")
+                );
+    }
+
+    public UUID getCurrentUserId() {
+
+        Authentication auth =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        CustomUserDetails principal =
+                (CustomUserDetails) auth.getPrincipal();
+
+        return principal.getUserId();
+    }
+}
