295d7dfbf9db1f2d8aef097a59dbdd70053199dd
lindtseyxvii <giousalvador@gmail.com>
2026-02-18 21:31:54 +0800

    feat: add PrescriptionDetailsDTO

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/prescription/PrescriptionDetailsDTO.java
@@ -0,0 +1,23 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescription;
+
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.PrescriptionItemResponseDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.user.UserDTO;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class PrescriptionDetailsDTO {
+
+    private UUID prescriptionId;
+    private LocalDate examDate;
+    private String notes;
+    private LocalDateTime createdAt;
+    private Boolean isArchived;
+    private UserDTO createdBy;
+
+    private List<PrescriptionItemResponseDTO> prescriptionItems;
+}
