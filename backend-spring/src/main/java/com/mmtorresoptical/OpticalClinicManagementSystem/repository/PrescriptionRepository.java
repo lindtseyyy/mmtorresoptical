96e8e153b302fa0791246a4968426285b71fbe6b
lindtseyxvii <giousalvador@gmail.com>
2026-02-18 21:33:28 +0800

    feat: add queries to retrieve the patient's prescriptions (archived, non-archived, both)

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/PrescriptionRepository.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/PrescriptionRepository.java
@@ -1,9 +1,15 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.repository;
 
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.Prescription;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface PrescriptionRepository extends JpaRepository<Prescription, UUID> {
+
+    Page<Prescription> findAllByIsArchivedFalseAndPatient_PatientId(UUID id, Pageable pageable);
+    Page<Prescription> findAllByIsArchivedTrueAndPatient_PatientId(UUID id, Pageable pageable);
+    Page<Prescription> findAllByPatient_PatientId(UUID id, Pageable pageable);
 }
