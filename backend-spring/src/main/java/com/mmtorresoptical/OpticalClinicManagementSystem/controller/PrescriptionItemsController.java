56c9acd3d0968d960dbfb58d68337b432eed7e9e
lindtseyxvii <giousalvador@gmail.com>
2026-02-19 18:31:43 +0800

    feat: add endpoint for retrieving prescription's items

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/controller/PrescriptionItemsController.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/controller/PrescriptionItemsController.java
@@ -2,14 +2,13 @@ package com.mmtorresoptical.OpticalClinicManagementSystem.controller;
 
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.CreatePrescriptionItemRequestDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.PrescriptionItemDetailsDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.services.ControllerService.PrescriptionItemsService;
 import com.mmtorresoptical.OpticalClinicManagementSystem.services.ControllerService.PrescriptionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +17,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class PrescriptionItemsController {
     private final PrescriptionService prescriptionService;
+    private final PrescriptionItemsService prescriptionItemsService;
 
     @PostMapping("/api/admin/prescriptions/{id}/prescription-items")
     public ResponseEntity<List<PrescriptionItemDetailsDTO>> addPrescriptionItems(@PathVariable UUID id, @Valid @RequestBody List<CreatePrescriptionItemRequestDTO> createPrescriptionRequestDTOList) {
@@ -25,4 +25,20 @@ public class PrescriptionItemsController {
 
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/api/admin/prescriptions/{id}/prescription-items")
+    public ResponseEntity<Page<PrescriptionItemDetailsDTO>> getAllPrescriptionItems(@PathVariable UUID id,
+                                                                                 @RequestParam(defaultValue = "0") int page,
+                                                                                 @RequestParam(defaultValue = "10") int size,
+                                                                                 @RequestParam(defaultValue = "createdAt") String sortBy,
+                                                                                 @RequestParam(defaultValue = "ascending") String sortOrder,
+                                                                                 @RequestParam(defaultValue = "ACTIVE") String archivedStatus) {
+
+
+        Page<PrescriptionItemDetailsDTO> prescriptionItemDetailsDTOPage = prescriptionItemsService.getAllPrescriptionItems(id, page, size, sortBy, sortOrder, archivedStatus);
+
+        return ResponseEntity.ok(prescriptionItemDetailsDTOPage);
+    }
+
 }
+
