54f72ea291391bed2c807e40ac95c1788cdf61a3
lindtseyxvii <giousalvador@gmail.com>
2026-02-19 21:47:25 +0800

    feat: add requestDTO-to-entity mapper function | add entity-to-responseDTO mapper function | add in-place update mapper function

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/UserMapper.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/UserMapper.java
@@ -1,11 +1,35 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.mapper;
 
-import com.mmtorresoptical.OpticalClinicManagementSystem.dto.user.UserDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.user.*;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.User;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserDTO entityToDTO(User user);
+
+    User requestDTOtoEntity(CreateUserRequestDTO requestDTO);
+
+    @Mapping(
+            target = "gender",
+            expression = "java(user.getGender().name())"
+    )
+    @Mapping(
+            target = "role",
+            expression = "java(user.getRole().name())"
+    )
+    UserResponseDTO entityToResponseDTO(User user);
+
+    @Mapping(
+            target = "gender",
+            expression = "java(user.getGender().name())"
+    )
+    @Mapping(
+            target = "role",
+            expression = "java(user.getRole().name())"
+    )
+    UserDetailsDTO entityToDetailsDTO(User user);
+
+    void updateEntityFromRequestDTO(UpdateUserRequestDTO userRequestDTO, @MappingTarget User user);
 }
