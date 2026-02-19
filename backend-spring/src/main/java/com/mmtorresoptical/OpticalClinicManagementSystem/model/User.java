1d611d8d9033649ebd9b645f2f890946dd93fb04
lindtseyxvii <giousalvador@gmail.com>
2026-02-19 21:48:47 +0800

    feat: add column names | add fullnamesortable field | change data types

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/model/User.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/model/User.java
@@ -4,8 +4,10 @@ import com.mmtorresoptical.OpticalClinicManagementSystem.enums.Gender;
 import com.mmtorresoptical.OpticalClinicManagementSystem.enums.Role;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
 import java.time.LocalDate;
-import java.time.OffsetDateTime;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -18,41 +20,47 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID userId;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
 
-    @Column(nullable = false)
+    @Column(name = "password_hash", nullable = false)
     private String passwordHash;
 
-    @Column(nullable = false)
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    @Column(name = "middle_name")
     private String middleName;
 
-    @Column(nullable = false)
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "gender", nullable = false)
     private Gender gender;
 
-    @Column(nullable = false)
+    @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column(name = "contact_number", nullable = false)
     private String contactNumber;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "role", nullable = false)
     private Role role; // "Admin" or "Staff"
 
-    @Column(updatable = false)
-    private OffsetDateTime createdAt = OffsetDateTime.now();
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
+    @Column(name = "is_archived", nullable = false)
     private Boolean isArchived = false;
+
+    @Column(name="full_name_sortable", nullable = false)
+    private String fullNameSortable;
 
     // Relationships
     @OneToMany(mappedBy = "user")
