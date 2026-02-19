1b92adfbe14503567171e125f1a5156cb47353d6
lindtseyxvii <giousalvador@gmail.com>
2026-02-19 18:13:52 +0800

    refactor: remove unused imports

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/prescription/UpdatePrescriptionRequestDTO.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/prescription/UpdatePrescriptionRequestDTO.java
@@ -1,11 +1,9 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescription;
 
-import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.PrescriptionItemRequestDTO;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Data
 public class UpdatePrescriptionRequestDTO {
