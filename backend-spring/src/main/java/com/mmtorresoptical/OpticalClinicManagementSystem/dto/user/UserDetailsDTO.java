7724c3c3618ce14d7a62367f7aecb9d878f49f7f
lindtseyxvii <giousalvador@gmail.com>
2026-02-19 21:47:35 +0800

    feat: add UserDetailsDTO

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/user/UserDetailsDTO.java
@@ -0,0 +1,23 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.dto.user;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+public class UserDetailsDTO {
+
+    private UUID userId;
+    private String firstName;
+    private String middleName; // Optional
+    private String lastName;
+    private String gender;
+    private LocalDate birthDate;
+    private String email;
+    private String contactNumber;
+    private String username;
+    private String role; // "Admin" or "Staff"
+    private Boolean isArchived = false;
+
+}
