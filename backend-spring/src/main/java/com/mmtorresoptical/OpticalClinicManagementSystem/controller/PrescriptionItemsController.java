537068772b94442640484fb81cfd110bec5fd7de
lindtseyxvii <giousalvador@gmail.com>
2026-02-19 18:12:29 +0800

    feat: add endpoint for creating single/multiple items to a prescription

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/controller/PrescriptionItemsController.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/controller/PrescriptionItemsController.java
@@ -1,14 +1,28 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.controller;
 
-import com.mmtorresoptical.OpticalClinicManagementSystem.repository.PrescriptionItemsRepository;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.CreatePrescriptionItemRequestDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.PrescriptionItemDetailsDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.services.ControllerService.PrescriptionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
+@RequiredArgsConstructor
 public class PrescriptionItemsController {
+    private final PrescriptionService prescriptionService;
 
-    private final PrescriptionItemsRepository prescriptionItemsRepository;
+    @PostMapping("/api/admin/prescriptions/{id}/prescription-items")
+    public ResponseEntity<List<PrescriptionItemDetailsDTO>> addPrescriptionItems(@PathVariable UUID id, @Valid @RequestBody List<CreatePrescriptionItemRequestDTO> createPrescriptionRequestDTOList) {
+        List<PrescriptionItemDetailsDTO> response = prescriptionService.addItems(id, createPrescriptionRequestDTOList);
 
-    PrescriptionItemsController(PrescriptionItemsRepository prescriptionItemsRepository) {
-        this.prescriptionItemsRepository = prescriptionItemsRepository;
+        return ResponseEntity.ok(response);
     }
 }
