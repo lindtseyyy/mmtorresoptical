c30a918965b6c83e38ee1eea610f4093af526b73
lindtseyxvii <giousalvador@gmail.com>
2026-02-19 19:03:41 +0800

    feat: add business logic for retrieving, updating, archiving, and restoring prescriptionItem,

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/services/ControllerService/PrescriptionItemsService.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/services/ControllerService/PrescriptionItemsService.java
@@ -1,9 +1,9 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.services.ControllerService;
 
-import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescription.PrescriptionListDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.PrescriptionItemDetailsDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.UpdatePrescriptionItemRequestDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.exception.ResourceNotFoundException;
 import com.mmtorresoptical.OpticalClinicManagementSystem.mapper.PrescriptionItemMapper;
-import com.mmtorresoptical.OpticalClinicManagementSystem.model.Prescription;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.PrescriptionItem;
 import com.mmtorresoptical.OpticalClinicManagementSystem.repository.PrescriptionItemsRepository;
 import lombok.RequiredArgsConstructor;
@@ -53,6 +53,49 @@ public class PrescriptionItemsService {
 
         // Map each of prescription entity to prescription listDTO and return
         return prescriptionItems.map(prescriptionItemMapper::entityToDetailsDTO);
+    }
+
+    public PrescriptionItemDetailsDTO getPrescription(UUID id) {
+        // Retrieve prescription or throw exception if not found
+        PrescriptionItem retrievedPrescriptionItem = prescriptionItemsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Prescription Item not found with id: " + id));
+
+        // Map prescription entity to prescription details DTO
+        PrescriptionItemDetailsDTO prescriptionDetailsDTO = prescriptionItemMapper.entityToDetailsDTO(retrievedPrescriptionItem);
+
+        return prescriptionDetailsDTO;
+    }
+
+    public PrescriptionItemDetailsDTO updatePrescription(UUID id, UpdatePrescriptionItemRequestDTO updatePrescriptionItemRequestDTO) {
+        // Retrieve prescription or throw exception if not found
+        PrescriptionItem retrievedPrescriptionItem = prescriptionItemsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Prescription Item not found with id: " + id));
+
+        prescriptionItemMapper.updatePrescriptionItemFromDTO(updatePrescriptionItemRequestDTO, retrievedPrescriptionItem);
+
+        PrescriptionItem updatedPrescriptionItem = prescriptionItemsRepository.save(retrievedPrescriptionItem);
+
+        return prescriptionItemMapper.entityToDetailsDTO(updatedPrescriptionItem);
+    }
+
+    public void archivePrescriptionItem(UUID id) {
+        // Retrieve prescription or throw exception if not found
+        PrescriptionItem retrievedPrescriptionItem = prescriptionItemsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Prescription Item not found with id: " + id));
+
+        retrievedPrescriptionItem.setIsArchived(true);
+
+        prescriptionItemsRepository.save(retrievedPrescriptionItem);
+    }
+
+    public void restorePrescriptionItem(UUID id) {
+        // Retrieve prescription or throw exception if not found
+        PrescriptionItem retrievedPrescriptionItem = prescriptionItemsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Prescription Item not found with id: " + id));
+
+        retrievedPrescriptionItem.setIsArchived(false);
+
+        prescriptionItemsRepository.save(retrievedPrescriptionItem);
     }
 
 }
