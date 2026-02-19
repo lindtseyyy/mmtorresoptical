9f207efd04d0b4256fd6fdc839becfe978990e68
lindtseyxvii <giousalvador@gmail.com>
2026-02-19 21:46:35 +0800

    feat: add queries for checking duplicate emails, name, contact number, username | add query for retrieving all unarchived users

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/UserRepository.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.repository;
 
+import com.mmtorresoptical.OpticalClinicManagementSystem.model.Prescription;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
@@ -8,8 +11,9 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    // For finding users for the ManageUsers page
-    List<User> findAllByIsArchivedFalse();
+
+    Page<User> findAllByIsArchivedFalse(Pageable pageable);
+    Page<User> findAllByIsArchivedTrue(Pageable pageable);
 
     // For checking conflicts during registration
     Optional<User> findByUsername(String username);
@@ -17,4 +21,9 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     // For login
     Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Boolean existsByFirstNameAndMiddleNameAndLastName(String firstName, String middleName, String lastName);
+    Boolean existsByEmail(String email);
+    Boolean existsByContactNumber(String contactNumber);
+    Boolean existsByUsername(String userName);
 }
