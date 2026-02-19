d7859e408501aeee601d52b94b21b46d87b6b2ac
lindtseyxvii <giousalvador@gmail.com>
2026-02-19 18:12:03 +0800

    feat: add entity-to-detailsDTO mapper function

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/PrescriptionItemMapper.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/PrescriptionItemMapper.java
@@ -1,6 +1,7 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.mapper;
 
-import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.PrescriptionItemRequestDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.CreatePrescriptionItemRequestDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.PrescriptionItemDetailsDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.PrescriptionItemResponseDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.PrescriptionItem;
 import org.mapstruct.Mapper;
@@ -12,7 +13,7 @@ import org.mapstruct.Mapping;
 )
 public interface PrescriptionItemMapper {
 
-    PrescriptionItem requestDTOtoEntity(PrescriptionItemRequestDTO requestDTO);
+    PrescriptionItem createRequestDTOtoEntity(CreatePrescriptionItemRequestDTO requestDTO);
 
     @Mapping(
             target = "correctionType",
@@ -35,4 +36,26 @@ public interface PrescriptionItemMapper {
             source = "user"
     )
     PrescriptionItemResponseDTO entityToResponseDTO(PrescriptionItem item);
+
+    @Mapping(
+            target = "correctionType",
+            expression = "java(item.getCorrectionType() != null ? item.getCorrectionType().name() : null)"
+    )
+    @Mapping(
+            target = "eyeSide",
+            expression = "java(item.getEyeSide() != null ? item.getEyeSide().name() : null)"
+    )
+    @Mapping(
+            target = "lensType",
+            expression = "java(item.getLensType() != null ? item.getLensType().name() : null)"
+    )
+    @Mapping(
+            target = "followUpStatus",
+            expression = "java(item.getFollowUpStatus() != null ? item.getFollowUpStatus().name() : null)"
+    )
+    @Mapping(
+            target = "createdBy",
+            source = "user"
+    )
+    PrescriptionItemDetailsDTO entityToDetailsDTO(PrescriptionItem item);
 }
