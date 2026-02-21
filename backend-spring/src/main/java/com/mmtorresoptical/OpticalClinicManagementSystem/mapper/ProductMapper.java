34be536968c95ae18df82b2d48f4ee21b9463e63
lindtseyxvii <giousalvador@gmail.com>
2026-02-21 20:10:34 +0800

    feat: add update entity inplace from updateProductRequest function

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/ProductMapper.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/ProductMapper.java
@@ -1,12 +1,9 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.mapper;
 
-import com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient.PatientDetailsDTO;
-import com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient.PatientRequestDTO;
-import com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient.PatientResponseDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.product.CreateProductRequestDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.product.ProductDetailsDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.product.ProductResponseDTO;
-import com.mmtorresoptical.OpticalClinicManagementSystem.model.Patient;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.product.UpdateProductRequestDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.Product;
 import org.mapstruct.*;
 
@@ -21,4 +18,5 @@ public interface ProductMapper {
 
     ProductDetailsDTO entityToDetailsDTO(Product product);
 
+    void updateProductFromUpdateRequestDTO(UpdateProductRequestDTO updateProductRequestDTO, @MappingTarget Product product);
 }
