7b8b7d83e184a0b0f7b3897a381152efa578da58
lindtseyxvii <giousalvador@gmail.com>
2026-02-21 20:11:22 +0800

    feat: remove unused query

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/ProductRepository.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/ProductRepository.java
@@ -3,17 +3,15 @@ import com.mmtorresoptical.OpticalClinicManagementSystem.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {
-    // Basic search query. You can make this more advanced with JpaSpecificationExecutor
-    @Query("SELECT p FROM Product p WHERE " +
-            "(LOWER(p.productName) LIKE LOWER(CONCAT('%', :query, '%')) OR CAST(p.productId AS STRING) LIKE :query) " +
-            "AND (:category IS NULL OR p.category = :category) " +
-            "AND (:supplier IS NULL OR p.supplier = :supplier)")
-    List<Product> searchProducts(String query, String category, String supplier);
+public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
+
+    Optional<Product> findByProductId(UUID id);
 
     // This finds all products that are NOT archived
     List<Product> findAllByIsArchivedFalse();
