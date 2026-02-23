7c4f2af5a2b812b13a7a14966fcedb26a551f526
lindtseyxvii <giousalvador@gmail.com>
2026-02-24 00:46:15 +0800

    feat: add Getter and Setter annotation feat: add transactionStatus, voidedAt, voidReason fields feat: add ManyToOne relationship with who voided the transaction chore: make the patient id optional feat: add orphan removal for transactionItems

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/model/Transaction.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/model/Transaction.java
@@ -1,9 +1,12 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.model;
 
 import com.mmtorresoptical.OpticalClinicManagementSystem.enums.PaymentType;
+import com.mmtorresoptical.OpticalClinicManagementSystem.enums.TransactionStatus;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
@@ -11,6 +14,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "transactions")
 public class Transaction {
@@ -21,7 +26,6 @@ public class Transaction {
     private UUID transactionId;
 
     @CreationTimestamp
-    @NotNull
     @Column(name = "transaction_date", nullable = false)
     private LocalDateTime transactionDate;
 
@@ -40,27 +44,43 @@ public class Transaction {
     private String referenceNumber;
 
     // OPTIONAL — for GCash / online payments
-    @Column(name = "gcash_payment_img", length = 255)
-    private String gcashPaymentImg;
+    @Column(name = "gcash_payment_img_dir", length = 255)
+    private String gcashPaymentImgDir;
 
     // OPTIONAL — for cash payments
     @Column(name = "cash_tender", precision = 10, scale = 2)
     private BigDecimal cashTender;
 
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    @Column(name = "transaction_status", nullable = false)
+    private TransactionStatus transactionStatus;
+
+    @Column(name = "voided_at")
+    private LocalDateTime voidedAt;
+
+    @Column(name = "void_reason", columnDefinition = "TEXT")
+    private String voidReason;
+
 
     // Relationship
     // Foreign Key — user (user_id)
+    @ManyToOne
+    @JoinColumn(name = "voided_by_user_id")
+    private User voidedBy;
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     // Foreign Key — patient (patient_id)
-    @ManyToOne
+    @ManyToOne(optional = true)
     @JoinColumn(name = "patient_id", nullable = false)
     private Patient patient;
 
     @OneToMany(mappedBy = "transaction",
-            cascade = CascadeType.ALL)
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
     private List<TransactionItem> transactionItems;
 }
 
