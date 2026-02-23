4883d1c1d8149545458d7e5ca62080a21dee154e
lindtseyxvii <giousalvador@gmail.com>
2026-02-23 23:55:25 +0800

    feat: add Refund entity

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/model/Refund.java
@@ -0,0 +1,35 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "refunds")
+public class Refund {
+
+    @Id
+    @GeneratedValue
+    private UUID refundId;
+
+    private Integer refundQuantity;
+
+    private String refundReason;
+
+    private LocalDateTime refundedAt;
+
+    private BigDecimal refundAmount;
+
+    @ManyToOne@JoinColumn(name = "user_id", nullable = false)
+    private User refundedBy;
+
+    @ManyToOne
+    @JoinColumn(name = "transaction_item_id", nullable = false)
+    private TransactionItem transactionItem;
+}
