ad5a40f609b260cce25fa611c0ff753efcc4dc22
lindtseyxvii <giousalvador@gmail.com>
2026-02-24 00:11:18 +0800

    feat: add custom Method Argument Type Mismatch exception handler

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,80 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.exception.handler;
+
+import com.mmtorresoptical.OpticalClinicManagementSystem.exception.custom.InsufficientStockException;
+import com.mmtorresoptical.OpticalClinicManagementSystem.exception.custom.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // For handling exceptions that violates NotValid annotation
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidation(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        ));
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errors);
+    }
+
+    // Custom not found exception
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handlePatientExists(
+            ResourceNotFoundException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(ex.getMessage());
+    }
+
+    // Custom Method Argument Type Mismatch exception
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleEnumError(
+            MethodArgumentTypeMismatchException ex
+    ) {
+
+        return ResponseEntity
+                .badRequest()
+                .body(
+                        "Invalid value for parameter: "
+                                + ex.getName()
+                );
+    }
+
+    // Insufficient stock exception
+    @ExceptionHandler(InsufficientStockException.class)
+    public ResponseEntity<String> handleStockError(
+            InsufficientStockException ex
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalStateError(
+            IllegalStateException ex
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+}
+
