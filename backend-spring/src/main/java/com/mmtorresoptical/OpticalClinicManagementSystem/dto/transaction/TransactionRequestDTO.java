9ab741f97277182c8e10a6231413e6143109c21b
lindtseyxvii <giousalvador@gmail.com>
2026-02-23 23:53:55 +0800

    feat: add TransactionRequestDTO

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/transaction/TransactionRequestDTO.java
@@ -0,0 +1,38 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.dto.transaction;
+
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.transactionitem.TransactionItemsRequestDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.enums.PaymentType;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class TransactionRequestDTO {
+
+    private UUID patientId;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private PaymentType paymentType;
+
+    // OPTIONAL — for GCash
+    @Column(name = "reference_number", length = 100)
+    private String referenceNumber;
+
+    // OPTIONAL — for GCash / online payments
+    @Column(name = "gcash_payment_img", length = 255)
+    private String gcashPaymentImg;
+
+    // OPTIONAL — for cash payments
+    @Column(name = "cash_tender", precision = 10, scale = 2)
+    private BigDecimal cashTender;
+
+    List<TransactionItemsRequestDTO> items;
+}
