9605cd36ff17c4b56d46516104d04f2fd6cab5dc
lindtseyxvii <giousalvador@gmail.com>
2026-02-23 23:53:48 +0800

    feat: add TransactionRepository

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/TransactionRepository.java
@@ -0,0 +1,13 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.repository;
+
+import com.mmtorresoptical.OpticalClinicManagementSystem.model.Product;
+import com.mmtorresoptical.OpticalClinicManagementSystem.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<Transaction, UUID>, JpaSpecificationExecutor<Transaction> {
+
+
+}
