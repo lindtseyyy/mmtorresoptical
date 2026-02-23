d768bbd99678ab40eb29156148888ba3d18cc4f2
lindtseyxvii <giousalvador@gmail.com>
2026-02-24 00:13:10 +0800

    feat: add entity-to-summaryDTO mapper function feat: add buildFullName function

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/PatientMapper.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/PatientMapper.java
@@ -3,6 +3,7 @@ package com.mmtorresoptical.OpticalClinicManagementSystem.mapper;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient.PatientDetailsDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient.PatientRequestDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient.PatientResponseDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.patient.PatientSummaryDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.Patient;
 import org.mapstruct.*;
 
@@ -29,4 +30,33 @@ public interface PatientMapper {
     @BeanMapping(nullValuePropertyMappingStrategy =
             NullValuePropertyMappingStrategy.IGNORE)
     void updatePatientFromDto(PatientRequestDTO patientRequest, @MappingTarget Patient patient);
+
+    @Mapping(
+            target = "fullName",
+            expression = "java(buildFullName(patient.getFirstName(), patient.getMiddleName(), patient.getLastName()))"
+
+    )
+    PatientSummaryDTO entityToSummaryDTO(Patient patient);
+
+    default String buildFullName(
+            String first,
+            String middle,
+            String last
+    ) {
+        StringBuilder name = new StringBuilder();
+
+        if (first != null && !first.isBlank()) {
+            name.append(first).append(" ");
+        }
+
+        if (middle != null && !middle.isBlank()) {
+            name.append(middle).append(" ");
+        }
+
+        if (last != null && !last.isBlank()) {
+            name.append(last);
+        }
+
+        return name.toString().trim();
+    }
 }
