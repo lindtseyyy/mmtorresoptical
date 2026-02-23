58b3006c4e55ac4a5f4f693520d2196d66bab6f5
lindtseyxvii <giousalvador@gmail.com>
2026-02-24 00:08:06 +0800

    refactor: change ConflictException directory

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/exception/custom/ConflictException.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/exception/custom/ConflictException.java
@@ -1,4 +1,4 @@
-package com.mmtorresoptical.OpticalClinicManagementSystem.exception;
+package com.mmtorresoptical.OpticalClinicManagementSystem.exception.custom;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
