62a7e198b39e8258c7fb6f7124a9156e852e95f7
lindtseyxvii <giousalvador@gmail.com>
2026-02-26 00:29:12 +0800

    feat: add resourceType, resourceId, detailsJSON fields

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/model/AuditLog.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/model/AuditLog.java
@@ -1,13 +1,21 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.model;
 
 import com.mmtorresoptical.OpticalClinicManagementSystem.enums.ActionType;
+import com.mmtorresoptical.OpticalClinicManagementSystem.enums.ResourceType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "audit_logs")
 public class AuditLog {
@@ -17,13 +25,27 @@ public class AuditLog {
     @Column(name = "log_id", updatable = false, nullable = false)
     private UUID logId;
 
-    @NotBlank
-    @Column(name = "action_type", nullable = false, length = 50)
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "action_type", nullable = false)
     private ActionType actionType;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "resource_type", nullable = false)
+    private ResourceType resourceType;
+
+    @Column(name = "resource_id", nullable = false)
+    private UUID resourceId;
 
     @NotBlank
     @Column(name = "details", columnDefinition = "TEXT")
     private String details;
+
+    @NotBlank
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "details_json", columnDefinition = "jsonb")
+    private String detailsJson;
 
     @CreationTimestamp
     @Column(name = "logged_at", nullable = false, updatable = false)
