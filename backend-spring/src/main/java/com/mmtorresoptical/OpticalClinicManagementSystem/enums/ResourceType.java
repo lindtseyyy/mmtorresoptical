142089958ae9b55f81960edd41705bc58a5d17b9
lindtseyxvii <giousalvador@gmail.com>
2026-02-26 00:27:42 +0800

    feat: add ResourceType enum

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/enums/ResourceType.java
@@ -0,0 +1,12 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.enums;
+
+public enum ResourceType {
+    PATIENT,
+    USER,
+    PRESCRIPTION,
+    PRESCRIPTION_ITEM,
+    HEALTH_HISTORY,
+    TRANSACTION,
+    TRANSACTION_ITEMS,
+    PRODUCT
+}
