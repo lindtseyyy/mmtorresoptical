d81a148940895022ccbe3c7b6c1be595c8864364
lindtseyxvii <giousalvador@gmail.com>
2026-02-26 00:38:05 +0800

    feat: add product entity-to-auditDTO mapper function feat: add product list entity-to-auditDTO list mapper function

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/ProductMapper.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/ProductMapper.java
@@ -1,8 +1,11 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.mapper;
 
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.audit.product.ProductAuditDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.product.*;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.Product;
 import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(
         componentModel = "spring"
@@ -16,6 +19,11 @@ public interface ProductMapper {
     ProductDetailsDTO entityToDetailsDTO(Product product);
 
     ProductSummaryDTO entityToSummaryDTO(Product product);
+
+    ProductAuditDTO entityToAuditDTO(Product product);
+
+    List<ProductAuditDTO> entityListToAuditDTOList(
+            List<Product> products);
 
     void updateProductFromUpdateRequestDTO(UpdateProductRequestDTO updateProductRequestDTO, @MappingTarget Product product);
 }
