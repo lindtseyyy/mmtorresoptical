9aa0586252bed5270eb7e7a19abc08512aeab9f8
lindtseyxvii <giousalvador@gmail.com>
2026-02-26 00:31:05 +0800

    feat: add ValidTransactionPayment annotation on class-level

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/transaction/TransactionRequestDTO.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/transaction/TransactionRequestDTO.java
@@ -2,6 +2,7 @@ package com.mmtorresoptical.OpticalClinicManagementSystem.dto.transaction;
 
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.transactionitem.TransactionItemsRequestDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.enums.PaymentType;
+import com.mmtorresoptical.OpticalClinicManagementSystem.validation.ValidTransactionPayment;
 import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -14,13 +15,18 @@ import java.util.List;
 import java.util.UUID;
 
 @Data
+@ValidTransactionPayment
 public class TransactionRequestDTO {
 
     private UUID patientId;
 
     @Enumerated(EnumType.STRING)
-    @NotNull
+    @NotNull(message = "Payment type is required")
     private PaymentType paymentType;
+
+    // OPTIONAL — for cash payments
+    @Column(name = "cash_tender", precision = 10, scale = 2)
+    private BigDecimal cashTender;
 
     // OPTIONAL — for GCash
     @Column(name = "reference_number", length = 100)
@@ -29,10 +35,6 @@ public class TransactionRequestDTO {
     // OPTIONAL — for GCash / online payments
     @Column(name = "gcash_payment_img", length = 255)
     private String gcashPaymentImg;
-
-    // OPTIONAL — for cash payments
-    @Column(name = "cash_tender", precision = 10, scale = 2)
-    private BigDecimal cashTender;
 
     List<TransactionItemsRequestDTO> items;
 }
