db3614996e295bcef8ad24d302dc4e07bf757ceb
lindtseyxvii <giousalvador@gmail.com>
2026-02-26 00:26:26 +0800

    feat: add PatientAuditDTO

---
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/dto/audit/patient/PatientAuditDTO.java
@@ -0,0 +1,22 @@
+package com.mmtorresoptical.OpticalClinicManagementSystem.dto.audit.patient;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class PatientAuditDTO {
+    private UUID patientId;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String gender;
+    private String contactNumber;
+    private String email;
+    private LocalDate birthDate;
+    private String address;
+    private Boolean isArchived;
+    private LocalDateTime createdAt;
+}
