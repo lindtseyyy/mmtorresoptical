ce9a2e13e096d036c5c75b7511121c75280e5163
lindtseyxvii <giousalvador@gmail.com>
2026-02-26 00:31:27 +0800

    feat: add query for checking if reference number exists

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/TransactionRepository.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/TransactionRepository.java
@@ -9,5 +9,6 @@ import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID>, JpaSpecificationExecutor<Transaction> {
 
+    Boolean existsByReferenceNumber(String referenceNumber);
 
 }
