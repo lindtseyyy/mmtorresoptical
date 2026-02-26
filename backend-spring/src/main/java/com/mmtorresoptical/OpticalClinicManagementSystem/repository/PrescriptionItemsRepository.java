c0319735a35bf6820f4ddc3412f54c3cca154e69
lindtseyxvii <giousalvador@gmail.com>
2026-02-26 23:16:51 +0800

    feat: remove unused queries

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/PrescriptionItemsRepository.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/PrescriptionItemsRepository.java
@@ -1,16 +1,15 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.repository;
 
+import com.mmtorresoptical.OpticalClinicManagementSystem.model.Prescription;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.PrescriptionItem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface PrescriptionItemsRepository extends JpaRepository<PrescriptionItem, UUID>{
+public interface PrescriptionItemsRepository extends JpaRepository<PrescriptionItem, UUID>, JpaSpecificationExecutor<PrescriptionItem> {
 
-    Page<PrescriptionItem> findAllByIsArchivedFalseAndPrescription_PrescriptionId(UUID id, Pageable pageable);
-    Page<PrescriptionItem> findAllByIsArchivedTrueAndPrescription_PrescriptionId(UUID id, Pageable pageable);
-    Page<PrescriptionItem> findAllByPrescription_PrescriptionId(UUID id, Pageable pageable);
 }
 
