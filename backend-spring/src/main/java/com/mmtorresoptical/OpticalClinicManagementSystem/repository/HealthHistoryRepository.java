6537719168bd59f75e79841af6a60647d541096a
lindtseyxvii <giousalvador@gmail.com>
2026-02-26 17:49:26 +0800

    refactor: remove unused queries and imports

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/HealthHistoryRepository.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/repository/HealthHistoryRepository.java
@@ -1,16 +1,11 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.repository;
 
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.HealthHistory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface HealthHistoryRepository extends JpaRepository<HealthHistory, UUID> {
+public interface HealthHistoryRepository extends JpaRepository<HealthHistory, UUID>, JpaSpecificationExecutor<HealthHistory> {
 
-    // For finding patient health history that are not archived
-    Page<HealthHistory> findAllByIsArchivedFalseAndPatient_PatientId(UUID patientId, Pageable pageable);
-
-    Page<HealthHistory> findAllByIsArchivedTrueAndPatient_PatientId(UUID patientId, Pageable pageable);
 }
