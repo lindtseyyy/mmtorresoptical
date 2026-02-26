d524bc3930e4dd2332796b25fd9725c7005e988a
lindtseyxvii <giousalvador@gmail.com>
2026-02-26 17:51:07 +0800

    feat: add prescription item entity-to-auditDTO mapper function feat: add prescription item entityList-to-AuditDTOList mapper function

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/PrescriptionItemMapper.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/PrescriptionItemMapper.java
@@ -1,11 +1,15 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.mapper;
 
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.audit.prescription.PrescriptionAuditDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.audit.prescriptionitem.PrescriptionItemAuditDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.CreatePrescriptionItemRequestDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.PrescriptionItemDetailsDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.PrescriptionItemResponseDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.prescriptionitems.UpdatePrescriptionItemRequestDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.PrescriptionItem;
 import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(
         componentModel = "spring",
@@ -58,6 +62,50 @@ public interface PrescriptionItemMapper {
             source = "user"
     )
     PrescriptionItemDetailsDTO entityToDetailsDTO(PrescriptionItem item);
+
+    @Mapping(
+            target = "correctionType",
+            expression = "java(item.getCorrectionType() != null ? item.getCorrectionType().name() : null)"
+    )
+    @Mapping(
+            target = "eyeSide",
+            expression = "java(item.getEyeSide() != null ? item.getEyeSide().name() : null)"
+    )
+    @Mapping(
+            target = "lensType",
+            expression = "java(item.getLensType() != null ? item.getLensType().name() : null)"
+    )
+    @Mapping(
+            target = "followUpStatus",
+            expression = "java(item.getFollowUpStatus() != null ? item.getFollowUpStatus().name() : null)"
+    )
+    @Mapping(
+            target = "createdByUserId",
+            source = "user.userId"
+    )
+    PrescriptionItemAuditDTO entityToAuditDTO(PrescriptionItem item);
+
+    @Mapping(
+            target = "correctionType",
+            expression = "java(item.getCorrectionType() != null ? item.getCorrectionType().name() : null)"
+    )
+    @Mapping(
+            target = "eyeSide",
+            expression = "java(item.getEyeSide() != null ? item.getEyeSide().name() : null)"
+    )
+    @Mapping(
+            target = "lensType",
+            expression = "java(item.getLensType() != null ? item.getLensType().name() : null)"
+    )
+    @Mapping(
+            target = "followUpStatus",
+            expression = "java(item.getFollowUpStatus() != null ? item.getFollowUpStatus().name() : null)"
+    )
+    @Mapping(
+            target = "createdByUserId",
+            source = "user.userId"
+    )
+    List<PrescriptionItemAuditDTO> entityListToAuditDTOList(List<PrescriptionItem> item);
 
     @BeanMapping(nullValuePropertyMappingStrategy =
             NullValuePropertyMappingStrategy.IGNORE)
