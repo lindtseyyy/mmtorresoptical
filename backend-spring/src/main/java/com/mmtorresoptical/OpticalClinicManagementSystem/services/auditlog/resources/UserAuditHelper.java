40b02adb64767c334ca36404e6d3ab5a7338e58b
lindtseyxvii <giousalvador@gmail.com>
2026-02-26 23:11:32 +0800

    refactor: change directory

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/services/auditlog/resources/UserAuditHelper.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/services/auditlog/resources/UserAuditHelper.java
@@ -1,4 +1,4 @@
-package com.mmtorresoptical.OpticalClinicManagementSystem.services.ControllerService.AuditLogService.resources;
+package com.mmtorresoptical.OpticalClinicManagementSystem.services.auditlog.resources;
 
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.audit.base.update.AuditUpdateEvent;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.audit.user.UserAuditDTO;
@@ -6,8 +6,8 @@ import com.mmtorresoptical.OpticalClinicManagementSystem.enums.ActionType;
 import com.mmtorresoptical.OpticalClinicManagementSystem.enums.ResourceType;
 import com.mmtorresoptical.OpticalClinicManagementSystem.mapper.UserMapper;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.User;
-import com.mmtorresoptical.OpticalClinicManagementSystem.services.ControllerService.AuditLogService.AuditLogHelper;
-import com.mmtorresoptical.OpticalClinicManagementSystem.services.ControllerService.AuditLogService.AuditLogService;
+import com.mmtorresoptical.OpticalClinicManagementSystem.services.auditlog.basehelper.AuditLogHelper;
+import com.mmtorresoptical.OpticalClinicManagementSystem.services.auditlog.AuditLogService;
 import com.mmtorresoptical.OpticalClinicManagementSystem.services.helper.JSONService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
