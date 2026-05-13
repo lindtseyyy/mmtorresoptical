4205d808a017dad25ba6a97f1993a7c35bc71172
lindtseyyy <giousalvador@gmail.com>
2026-05-14 00:46:02 +0800

    feat: add isDiscounted mapping to TransactionItemResponseDTO and TransactionItemDetailsDTO

--- backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/TransactionItemMapper.java
+++ backend-spring/src/main/java/com/mmtorresoptical/OpticalClinicManagementSystem/mapper/TransactionItemMapper.java
@@ -1,6 +1,7 @@
 package com.mmtorresoptical.OpticalClinicManagementSystem.mapper;
 
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.audit.transactionitem.TransactionItemAuditDTO;
+import com.mmtorresoptical.OpticalClinicManagementSystem.dto.transactionitem.TransactionItemDetailsDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.transactionitem.TransactionItemResponseDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.dto.transactionitem.TransactionItemsRequestDTO;
 import com.mmtorresoptical.OpticalClinicManagementSystem.model.Transaction;
@@ -22,10 +23,24 @@ public interface TransactionItemMapper {
             expression = "java(transactionItem.getDiscountType() != null ? transactionItem.getDiscountType().name() : null)"
     )
     @Mapping(
+            target = "isDiscounted",
+            expression = "java(transactionItem.getDiscountType() != null)"
+    )
+    @Mapping(
             target = "subtotal",
             source = "subtotal"
     )
     TransactionItemResponseDTO entityToResponseDTO(TransactionItem transactionItem);
+
+    @Mapping(
+            target = "discountType",
+            expression = "java(transactionItem.getDiscountType() != null ? transactionItem.getDiscountType().name() : null)"
+    )
+    @Mapping(
+            target = "isDiscounted",
+            expression = "java(transactionItem.getDiscountType() != null)"
+    )
+    TransactionItemDetailsDTO entityToDetailsDTO(TransactionItem transactionItem);
 
 
     @Mapping(
